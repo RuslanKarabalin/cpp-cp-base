c840afe4d92a0549659816fd66d5ae151a9209ef
Ruslan Karabalin <karabalin112@gmail.com>
2025-11-25 16:56:45 +0300

    feat: add krt with Comparable and Stack

--- template/template.cpp
+++ template/template.cpp
@@ -1,6 +1,74 @@
 #include <iostream>
 #include <vector>
 
+// Namespace for my developments
+namespace krt {
+    // Concept for comparable types
+    template <typename T>
+    concept Comparable = requires(const T& a, const T& b) {
+        { a <  b } -> std::convertible_to<bool>;
+        { a <= b } -> std::convertible_to<bool>;
+        { a >  b } -> std::convertible_to<bool>;
+        { a >= b } -> std::convertible_to<bool>;
+        { a == b } -> std::convertible_to<bool>;
+        { a != b } -> std::convertible_to<bool>;
+    };
+
+    // Stack based on std::vector
+    template <typename T>
+    class Stack {
+    public:
+        bool empty() const {
+            return data.empty();
+        }
+
+        std::size_t size() const {
+            return data.size();
+        }
+
+        void push(const T& value) {
+            data.push_back(value);
+        }
+
+        void push(T&& value) {
+            data.push_back(std::move(value));
+        }
+
+        void pop() {
+            if (empty()) {
+                throw std::out_of_range("Empty stack!");
+            }
+            data.pop_back();
+        }
+
+        T& top() {
+            if (empty()) {
+                throw std::out_of_range("Empty stack!");
+            }
+            return data.back();
+        }
+
+    // Standard operator<< overloading for krt::Stack<T>
+    // From "top" to "bottom"
+    friend std::ostream& operator<<(std::ostream& out, const Stack<T>& s) {
+        out << "[";
+        for (size_t i = 0; i < s.data.size(); ++i) {
+            out << s.data[s.data.size() - i - 1];
+            if (i < s.data.size() - 1) {
+                out << ", ";
+            }
+        }
+        out << "]";
+        return out;
+    }
+
+    private:
+        std::vector<T> data;
+    };
+
+}
+
+// Standard operator<< overloading for std::vector<T>
 template <typename T>
 std::ostream &operator<<(std::ostream &out, const std::vector<T> &v) {
     out << "[";
@@ -23,14 +91,17 @@ int main() {
     size_t n;
     std::cin >> n;
 
-    std::vector<int> vec;
+    std::vector<int> vector;
+    krt::Stack<int> krtStack;
 
     for (size_t i = 0; i < n; ++i) {
         int v;
         std::cin >> v;
-        vec.emplace_back(v);
+        vector.push_back(v);
+        krtStack.push(v);
     }
 
-    std::cout << vec << std::endl;
+    std::cout << "std::vector<T>: " << vector << std::endl;
+    std::cout << "krt::Stack<T>: " << krtStack << std::endl;
     return 0;
 }
